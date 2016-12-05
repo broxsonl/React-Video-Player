c10be7d2d4cfd958d54d42f3e37b149f8e6f1ac7
Lee Broxson <broxsonl@gmail.com>
2016-12-05 12:49:44 -0800

    Built a hello world with jsx and react. :)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+'use strict';
 
-import App from './components/app';
-import reducers from './reducers';
+const React = require('react');
+const ReactDOM = require('react-dom');
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = () => {
+  return <div>Hello world!</div>;
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
