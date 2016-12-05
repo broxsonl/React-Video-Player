7178f9af4bfa57f99d6478e56e976ab2c4531acc
Lee Broxson <broxsonl@gmail.com>
2016-12-05 13:23:34 -0800

    component files created. search-bar initially created.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 'use strict';
 
-const React = require('react');
-const ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search-bar.js';
+
+const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
 
 const App = () => {
   return <div>Hello world!</div>;
-}
+};
+
+
 
 ReactDOM.render(<App />, document.querySelector('.container'));
