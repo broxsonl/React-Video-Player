059d3e332e3a0f79b793b4448ae512591d6dadb9
Lee Broxson <broxsonl@gmail.com>
2016-12-21 23:34:53 -0800

    Added onInputChange event handler for onchange event on Searchbar component.

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -7,7 +7,13 @@ const Component = React.Component;
 
 class SearchBar extends Component {
   render() {
-    return <input />
+    // The below html element will emit a change event, and that will run our onInputChange handler when it does.
+    return <input onChange={this.onInputChange} />
+  }
+
+  //event handler. Whenever the input changes, run the code inside
+  onInputChange() {
+    
   }
 }
 
