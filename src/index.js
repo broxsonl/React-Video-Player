ea289e04a37b1582ff82385b626b41af727bc281
Lee Broxson <broxsonl@gmail.com>
2016-12-22 18:07:01 -0800

    Created video list and video li. Have video icontent coming onto the screen.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search-bar';
+import VideoList from './components/video-list';
 
 const YOUTUBE_API_KEY = require('../env.js')
 
@@ -13,14 +14,16 @@ class App extends Component {
 
     this.state = { videos: [] };
 
-    YTSearch({key: YOUTUBE_API_KEY, term: 'surfboards'}, function(data) {
-      console.log(data);
-    })
+    YTSearch({key: YOUTUBE_API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos: videos});
+    });
   }
+
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
